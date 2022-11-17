990db6e3e6a892424af1aee59cc24b6e4d9a1052
mahiMathewsCov <mathewsm@uni.coventry.ac.uk>
2022-11-17 10:02:36 +0000

    Signin comment fix

--- app/src/main/java/com/example/bookstore/Signin.java
+++ app/src/main/java/com/example/bookstore/Signin.java
@@ -44,3 +44,99 @@ public class Signin extends AppCompatActivity {
 
     }
 }
+
+//package com.example.bookstoreapp;
+//
+//import android.app.ProgressDialog;
+//import android.os.Bundle;
+//
+//import com.example.bookstoreapp.databinding.SigninMainBinding;
+//import com.google.android.gms.tasks.OnFailureListener;
+//import com.google.android.gms.tasks.OnSuccessListener;
+//import com.google.android.material.button.MaterialButton;
+//import com.google.firebase.auth.AuthResult;
+//import com.google.firebase.auth.FirebaseAuth;
+//
+//import androidx.annotation.NonNull;
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.view.View;
+//
+//import android.widget.TextView;
+//import android.widget.Toast;
+//
+//public class SigninActivity extends AppCompatActivity {
+//
+//SigninMainBinding binding;
+//    FirebaseAuth firebaseAuth;
+//    ProgressDialog progressDialog;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        binding=SigninMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//        firebaseAuth=FirebaseAuth.getInstance();
+//        progressDialog=new ProgressDialog(this);
+//
+//
+//
+//        binding.login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String email=binding.username.getText().toString().trim();
+//                String password=binding.password.getText().toString();
+//                progressDialog.show();
+//
+//                firebaseAuth.signInWithEmailAndPassword(email,password)
+//                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+//                            @Override
+//                            public void onSuccess(AuthResult authResult) {
+//                                progressDialog.cancel();
+//                                Toast.makeText(SigninActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+//
+//                            }
+//                        })
+//                        .addOnFailureListener(new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//                                progressDialog.cancel();
+//                                Toast.makeText(SigninActivity.this,e.getMessage(), Toast.LENGTH_SHORT).show();
+//
+//                            }
+//                        });
+//            }
+//        });
+//
+//        binding.forgot.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String email=binding.username.getText().toString();
+//                progressDialog.setTitle("Sending Mail");
+//                progressDialog.show();
+//
+//                firebaseAuth.sendPasswordResetEmail(email)
+//                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                            @Override
+//                            public void onSuccess(Void unused) {
+//                                progressDialog.cancel();
+//                                Toast.makeText(SigninActivity.this,"Email sent", Toast.LENGTH_SHORT).show();
+//
+//                            }
+//                        })
+//                        .addOnFailureListener(new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//                                progressDialog.cancel();
+//                                Toast.makeText(SigninActivity.this,e.getMessage(), Toast.LENGTH_SHORT).show();
+//
+//                            }
+//                        });
+//            }
+//        });
+//
+//
+//
+//
+//    }
+//}
